6c9274febaaba3b3d307053f88e536ee0e3e2182
lemonomg <myx0930@163.com>
2021-03-11 13:53:30 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Component from 'vue-class-component'
 
 Vue.config.productionTip = false
-
+// 注册组件路由钩子
+Component.registerHooks([
+  'beforeRouteEnter',
+  'beforeRouteLeave',
+  'beforeRouteUpdate'
+]);
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')

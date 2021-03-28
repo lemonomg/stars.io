3eef1976b978e48982d9d9d5f273e1986523bc17
lemonomg <myx0930@163.com>
2021-03-28 14:07:05 +0800

    完成登录

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -54,10 +54,7 @@ export default class Login extends Vue {
   /** 登录 */
   private handleLogin() {
     if (this.checkLogin()) {
-      Toast.loading({
-        message: "登录中...",
-        forbidClick: true,
-      });
+      Toast.loading();
       this.login(this.loginForm).then(() => {
         this.$router.push({ path: "/my" });
         Toast.clear();
